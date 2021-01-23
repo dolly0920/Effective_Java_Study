191ea58696fbc92d16430c02d811a70a5e5c15ae
soyeon <sypark9646@gmail.com>
2021-01-23 10:15:03 +0900

    feat: item3 singleton create second object exception

--- src/sypark9646/src/main/java/com/example/demo/item03/EagerInitializationSingleton.java
+++ src/sypark9646/src/main/java/com/example/demo/item03/EagerInitializationSingleton.java
@@ -5,6 +5,9 @@ public class EagerInitializationSingleton {
 	private static EagerInitializationSingleton instance = new EagerInitializationSingleton();
 
 	private EagerInitializationSingleton() {
+		if (instance != null) {
+			throw new InstantiationError("Creating of this object is not allowed.");
+		}
 	}
 
 	public static EagerInitializationSingleton getInstance() {
