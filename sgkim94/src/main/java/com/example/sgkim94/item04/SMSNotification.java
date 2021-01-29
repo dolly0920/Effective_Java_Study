4c28ec31b748a924aff48b8553d132f0cb02dc34
sanggu <tkdrn8578@naver.com>
2021-01-30 00:26:10 +0900

    feat: create SMSNotification that has SMS type

---
+++ sgkim94/src/main/java/com/example/sgkim94/item04/SMSNotification.java
@@ -0,0 +1,9 @@
+package com.example.sgkim94.item04;
+
+public class SMSNotification implements Notification {
+    @Override
+    public String notifyToUser() {
+        return "Check SMS!";
+    }
+
+}
