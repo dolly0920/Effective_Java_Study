bbaf5bb8e4f7f447d240a2e1ec2275c500e9ac44
sanggu <tkdrn8578@naver.com>
2021-01-30 00:26:26 +0900

    feat: create TimeoutNotification that has Timeout type

---
+++ sgkim94/src/main/java/com/example/sgkim94/item04/TimeoutNotification.java
@@ -0,0 +1,9 @@
+package com.example.sgkim94.item04;
+
+public class TimeoutNotification implements Notification {
+    @Override
+    public String notifyToUser() {
+        return "Check Time!";
+    }
+
+}
