070fcacec0ac7da35c832d5cd6805efa1272040f
sanggu <tkdrn8578@naver.com>
2021-01-30 00:25:38 +0900

    feat: create PrintNotification that has print type

---
+++ sgkim94/src/main/java/com/example/sgkim94/item04/PrintNotification.java
@@ -0,0 +1,8 @@
+package com.example.sgkim94.item04;
+
+public class PrintNotification implements Notification {
+    @Override
+    public String notifyToUser() {
+        return "Check Print!";
+    }
+}
