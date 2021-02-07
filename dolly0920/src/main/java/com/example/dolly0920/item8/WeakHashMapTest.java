fab6daaaa45076d25bad40f49fb3195d838a684f
dolly0920 <dolly0920@naver.com>
2021-02-07 09:32:13 +0900

    If key reference is null, hashmap vs. weakhashmap

--- dolly0920/src/main/java/com/example/dolly0920/item8/WeakHashMapTest.java
+++ dolly0920/src/main/java/com/example/dolly0920/item8/WeakHashMapTest.java
@@ -7,9 +7,7 @@ public class WeakHashMapTest {
 
     public static void main(String[] args) {
         // simulation 1 : <hashmap>> when key reference will be null, verify using memory.
-        // simulation 2 : <weakhashmap> when key reference will be null, verify using memory.
         WeakHashMap<Integer, String> weakHashMap = new WeakHashMap<>();
-        HashMap<Integer, String> hashMap = new HashMap<>();
 
         Integer key1 = 1000;
         Integer key2 = 2000;
@@ -17,15 +15,10 @@ public class WeakHashMapTest {
         weakHashMap.put(key1, "test1");
         weakHashMap.put(key2, "test2");
 
-        //hashMap.put(key1, "test3");
-        //hashMap.put(key2, "test4");
-
         key1 = null;
-        //key2 = null;
 
-        System.gc();  //강제 Garbage Collection
+        System.gc();  //Forced Garbage Collection
 
         weakHashMap.entrySet().stream().forEach(el -> System.out.println(el));
-        //hashMap.entrySet().stream().forEach(el -> System.out.println(el));
     }
 }
