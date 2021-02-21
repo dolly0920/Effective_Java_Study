efae35e12d543e9b1bac48ef083c7059a52145ca
soyeon <sypark9646@gmail.com>
2021-02-22 03:17:10 +0900

    refactor: public 클래스의 public 필드 -> private 으로 수정

--- sypark9646/src/main/java/com/example/sypark9646/item13/Child.java
+++ sypark9646/src/main/java/com/example/sypark9646/item13/Child.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 @Getter
 public class Child extends Parent {
 
-		public int childField;
+		private int childField;
 
 		public Child(String parentField, int childField) {
 				super(parentField);
