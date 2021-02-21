756cfbd278372e7325dfda9e0feb1362f262a7e6
VSFe <klm03025@gmail.com>
2021-02-21 16:35:52 +0900

    feat: complete item13, 14 with test code

--- vsfe/src/main/java/com/example/vsfe/item13_14/Movie.java
+++ vsfe/src/main/java/com/example/vsfe/item13_14/Movie.java
@@ -2,13 +2,28 @@ package com.example.vsfe.item13_14;
 
 import lombok.AllArgsConstructor;
 
+import java.util.Comparator;
+
+import static java.util.Comparator.comparing;
+
 @AllArgsConstructor
-public class Movie implements Cloneable {
+public class Movie implements Cloneable, Comparable<Movie> {
     String name;
     String director;
     String[] staffs;
     int release_year;
     Nation nation;
+
+    private static final Comparator<Movie> COMPARATOR =
+            comparing((Movie mv) -> mv.name)
+            .thenComparingInt(mv -> mv.release_year)
+            .thenComparing(mv -> mv.director)
+            .thenComparing(mv -> mv.nation);
+
+    @Override
+    public int compareTo(Movie o) {
+        return COMPARATOR.compare(this, o);
+    }
 
     @Override
     public Movie clone() {
--- vsfe/src/test/java/com/example/vsfe/item13_14/MovieTest.java
+++ vsfe/src/test/java/com/example/vsfe/item13_14/MovieTest.java
@@ -1,6 +1,39 @@
 package com.example.vsfe.item13_14;
 
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MovieTest {
-    Movie movie1 = new Movie("AA", "BBB", {}, 2020, Nation.KOREA);
+    Movie movie1 = new Movie("AA", "BBB", new String[] {"A", "B", "C"}, 2020, Nation.KOREA);
+    Movie movie2 = new Movie("AA", "BBB", new String[] {"S", "B", "C"}, 2019, Nation.KOREA);
+    Movie movie3 = new Movie("BB", "CCC", new String[] {"Y", "O", "N"}, 2015, Nation.KOREA);
+    Movie movie4 = new Movie("BB", "DDD", new String[] {"U", "S", "A"}, 2015, Nation.USA);
+    Movie movie5 = new Movie("CC", "EEE", new String[] {"J", "B", "C"}, 2020, Nation.KOREA);
+    Movie movie6 = new Movie("CC", "EEE", new String[] {"J", "B", "C"}, 2020, Nation.KOREA);
+
+    @Test
+    public void compareTest() {
+        assertTrue(movie1.compareTo(movie2) > 0);
+        assertTrue(movie1.compareTo(movie3) < 0);
+        assertTrue(movie3.compareTo(movie4) < 0);
+        assertTrue(movie5.compareTo(movie6) == 0);
+    }
+
+    @Test
+    public void cloneTest() {
+        Movie newMovie = movie1.clone();
+
+        assertEquals(false, movie1 == newMovie);
+
+        assertEquals(true, movie1.name.equals(newMovie.name));
+        assertEquals(true, movie1.director.equals(newMovie.director));
+        assertEquals(false, movie1.staffs.equals(newMovie.staffs));
+        assertEquals(true, Arrays.equals(movie1.staffs, newMovie.staffs));
+
+    }
 }
