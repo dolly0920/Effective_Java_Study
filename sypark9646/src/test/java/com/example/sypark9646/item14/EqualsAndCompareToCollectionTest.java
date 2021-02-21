ea9b496790a9c0d8f04a9899a9db1cc815d1b181
soyeon <sypark9646@gmail.com>
2021-02-22 03:24:52 +0900

    refactor: optimize imports

--- sypark9646/src/test/java/com/example/sypark9646/item14/EqualsAndCompareToCollectionTest.java
+++ sypark9646/src/test/java/com/example/sypark9646/item14/EqualsAndCompareToCollectionTest.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.TreeSet;
 import org.junit.jupiter.api.Assertions;
