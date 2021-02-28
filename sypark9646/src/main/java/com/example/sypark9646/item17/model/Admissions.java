890a9e5d70bf7401a14422de60c56b4f657036c8
soyeon <sypark9646@gmail.com>
2021-02-28 14:02:45 +0900

    refactor: cache accepted/declined list

--- sypark9646/src/main/java/com/example/sypark9646/item17/model/Admissions.java
+++ sypark9646/src/main/java/com/example/sypark9646/item17/model/Admissions.java
@@ -1,8 +1,8 @@
 package com.example.sypark9646.item17.model;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
 import com.example.sypark9646.item17.utils.Messages;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -12,11 +12,27 @@ public class Admissions {
 
     public static final int MAX_APPLY_SIZE = 6;
     private List<Admission> admissions;
+    private List<Admission> accepted = new ArrayList<>();
+    private List<Admission> declined = new ArrayList<>();
+    private String result;
 
     public Admissions(List<Admission> admissions) {
         validateSize(admissions);
         validateDuplicate(admissions);
         this.admissions = admissions;
+
+        admissions.forEach(
+            admission -> {
+                if (admission.isAccepted()) {
+                    this.accepted.add(admission);
+                } else {
+                    this.declined.add(admission);
+                }
+            }
+        );
+
+        this.result = accepted.size() != 0 ? Messages.SHOW_RESULT_PASS_MESSAGE.getMessage()
+            : Messages.SHOW_RESULT_NO_PASS_MESSAGE.getMessage();
     }
 
     private void validateSize(List<Admission> admissions) {
@@ -33,30 +49,21 @@ public class Admissions {
     }
 
     public void showResult() {
-        boolean isAccepted = admissions.stream().anyMatch(Admission::isAccepted);
-
-        getAcceptedResult();
-        String message =
-            isAccepted ? Messages.SHOW_RESULT_PASS_MESSAGE.getMessage()
-                : Messages.SHOW_RESULT_NO_PASS_MESSAGE.getMessage();
-        System.out.println(message);
+        admissions.forEach(
+            System.out::println
+        );
+        System.out.println(result);
     }
 
     public void getAcceptedResult() {
         System.out.println(Messages.PASS_LIST.getMessage());
         System.out.println(Messages.DELIMITER.getMessage());
-        AtomicBoolean isAccepted = new AtomicBoolean(false);
 
-        admissions.forEach(
-            admission -> {
-                if (admission.isAccepted()) {
-                    System.out.println(admission);
-                    isAccepted.set(true);
-                }
-            }
+        accepted.forEach(
+            System.out::println
         );
 
-        if (!isAccepted.get()) {
+        if (accepted.size() == 0) {
             System.out.println(Messages.NO_PASS_LIST.getMessage());
         }
         System.out.println(Messages.DELIMITER.getMessage());
@@ -65,18 +72,12 @@ public class Admissions {
     public void getDeclinedResult() {
         System.out.println(Messages.FAIL_LIST.getMessage());
         System.out.println(Messages.DELIMITER.getMessage());
-        AtomicBoolean isDeclined = new AtomicBoolean(false);
 
-        admissions.forEach(
-            admission -> {
-                if (!admission.isAccepted()) {
-                    System.out.println(admission);
-                    isDeclined.set(true);
-                }
-            }
+        declined.forEach(
+            System.out::println
         );
 
-        if (!isDeclined.get()) {
+        if (declined.size() == 0) {
             System.out.println(Messages.NO_FAIL_LIST.getMessage());
         }
         System.out.println(Messages.DELIMITER.getMessage());
