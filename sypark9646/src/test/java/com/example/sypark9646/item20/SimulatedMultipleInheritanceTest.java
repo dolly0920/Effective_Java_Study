edd48f01476a2d0655ee6959933ae221b6adcaf4
soyeon <sypark9646@gmail.com>
2021-03-11 13:53:58 +0900

    add: truck 테스트 추가

--- sypark9646/src/test/java/com/example/sypark9646/item20/SimulatedMultipleInheritanceTest.java
+++ sypark9646/src/test/java/com/example/sypark9646/item20/SimulatedMultipleInheritanceTest.java
@@ -14,5 +14,6 @@ public class SimulatedMultipleInheritanceTest {
         pizzaTruck.turnLightOn();
         pizzaTruck.cook(pizza);
         pizzaTruck.turnLightOff();
+        pizzaTruck.driveTo("home");
     }
 }
